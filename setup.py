287725c3b268c092cfe36bf1a7aed15597d5b95c
VED PRAKASH <153211377+VED-PRAKA5H@users.noreply.github.com>
2024-08-16 23:07:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path: str) -> List[str]:
 
         return requirements
 setup(
-    name = "HousePrediction",
+    name = "performanceprediction",
     version = '0.0.1',
     author = 'ved',
     author_email = 'vihastvideos21@gmail.com',
