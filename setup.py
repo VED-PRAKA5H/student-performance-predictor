aa16fb253fa7fefc4b78445cf447d281388af4eb
VED-PRAKA5H <vihastvideos21@gmail.com>
2025-03-26 16:38:37 +0530

    setup environments

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
-from setuptools import find_packages, setup
-from typing import List
+from setuptools import find_packages, setup  # Package discovery and setup tools
+from typing import List  # Type hints support
 
+# Constant for editable installation marker
 HYPHEN_E_DOT = '-e .'
 
 
 def get_requirements(file_path: str) -> List[str]:
-    '''
-    this function will return the list of requirements
-    '''
+    """
+    Read and parse requirements from a text file.
+
+    Args:
+        file_path (str): Path to requirements.txt file
+
+    Returns:
+        List[str]: Clean list of requirements excluding empty lines and -e .
+    """
     requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+        # Read all lines and remove whitespace/newline characters
+        requirements = [req.strip() for req in file_obj.readlines()]
 
+        # Remove editable installation marker if present
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
 
-        return requirements
+    return requirements
+
+
+# Package configuration
 setup(
-    name = "performanceprediction",
-    version = '0.0.1',
-    author = 'ved',
-    author_email = 'vihastvideos21@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
-    )
-    
+    name="performance_prediction",  # Package name ( PEP 8 recommends lowercase with underscores )
+    version='0.0.1',  # Initial version (semantic versioning recommended)
+    author='Ved',  # Author name
+    author_email='vihast@gmail.com',  # not real gmail
+    packages=find_packages(),  # Automatically discover Python packages in directory
+    install_requires=get_requirements('requirements.txt'),  # Runtime dependencies
+)
 
