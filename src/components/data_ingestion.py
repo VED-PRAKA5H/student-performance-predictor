2974009cd80df8c5807f48c02aa729d5f2f9b525
VED-PRAKA5H <vihastvideos21@gmail.com>
2025-03-26 16:45:09 +0530

    Implement data ingestion to gather and preprocess data

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,62 +1,73 @@
-import os
-import sys
-from src.exception import CustomException
-from src.logger import logging
-import pandas as pd
+import os  # Module for interacting with the operating system
+import sys  # Module for system-specific parameters and functions
+from src.exception import CustomException  # Custom exception handling
+from src.logger import logging  # Logging module for tracking events
+import pandas as pd  # Library for data manipulation and analysis
+from sklearn.model_selection import train_test_split  # Function to split datasets into training and testing sets
+from dataclasses import dataclass  # Decorator for creating data classes
+from src.components.data_transformation import DataTransformation  # Data transformation component
+from src.components.model_trainer import ModelTrainer  # Model training component
 
-from sklearn.model_selection import train_test_split
-from dataclasses import dataclass  # to directly define class variable without __init__ using decorator @dataclass
-
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
-
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
-    # if only defining variables then its okay to use dataclass, if have methods prefer init approach
-    train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','test.csv')
-    raw_data_path: str=os.path.join('artifacts','data.csv')
+    """
+    Configuration class for data ingestion.
+    Uses a dataclass to define paths for train, test, and raw data.
+    """
+    train_data_path: str = os.path.join('artifacts', 'train.csv')  # Path to save training data
+    test_data_path: str = os.path.join('artifacts', 'test.csv')  # Path to save testing data
+    raw_data_path: str = os.path.join('artifacts', 'stud.csv')  # Path to save raw data
+
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+        """Initialize the DataIngestion class and set up configuration."""
+        self.ingestion_config = DataIngestionConfig()  # Create an instance of the configuration class
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method")
+        """Method to perform data ingestion."""
+        logging.info("Entered the data ingestion method")  # Log entry into the method
         try:
-            df = pd.read_csv('../../Notebook/data/stud.csv')
-            logging.info('Read the dataset as dataframe')
+            # Read the CSV file into a pandas DataFrame
+            df = pd.read_csv('../../notebook/data/stud.csv')
+            logging.info('Read the dataset as dataframe')  # Log successful reading of the dataset
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            # Create directories if they do not exist for saving files
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False, header=True)
+            # Save the raw data to the specified path
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info("Train test split initiated")
-            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+            logging.info("Train test split initiated")  # Log initiation of train-test split
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+            # Split the dataset into training and testing sets (80% train, 20% test)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-            logging.info("Ingestion of data is completed")
+            # Save the training set to CSV file
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            # Save the testing set to CSV file
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
-            return(
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+            logging.info("Ingestion of data is completed")  # Log completion of data ingestion
+
+            return (
+                self.ingestion_config.train_data_path,  # Return path to training data
+                self.ingestion_config.test_data_path  # Return path to testing data
             )
 
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)  # Raise a custom exception if an error occurs
+
 
 if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
+    obj = DataIngestion()  # Create an instance of DataIngestion class
+    train_data, test_data = obj.initiate_data_ingestion()  # Initiate data ingestion process
 
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation = DataTransformation()  # Create an instance of DataTransformation class
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,
+                                                                              test_data)  # Transform the datasets
 
-    model_trainer = ModelTrainer()
-    acc=model_trainer.initiate_model_trainer(train_arr,test_arr)
-    print(acc)
+    model_trainer = ModelTrainer()  # Create an instance of ModelTrainer class
+    acc = model_trainer.initiate_model_trainer(train_arr, test_arr)  # Train the model and get accuracy
+    # print(acc)  # Print the accuracy of the trained model
